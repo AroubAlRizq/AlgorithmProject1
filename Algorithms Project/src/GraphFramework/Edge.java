2236a6acb57bfc04134487993e08ceed4c4912b2
AroubAlRizq <127698586+AroubAlRizq@users.noreply.github.com>
2023-05-20 14:20:09 +0300

    Update Edge.java

--- Algorithms Project/src/GraphFramework/Edge.java
+++ Algorithms Project/src/GraphFramework/Edge.java
@@ -8,30 +8,30 @@ package GraphFramework;
  * Project Code
  * 18th may. 2023
  */
-public class Edge implements Comparable<Edge> {
+public class Edge implements Comparable<Edge> { // class to store weights
 
-    int weight;
-    Vertex source;
-    Vertex target;
-    Vertex parent;
+    int weight; // value of egde weight
+    Vertex source; // which vertex the edge is coming from
+    Vertex target; // which vertex the edge arrives at
+    Vertex parent; 
 
-    public Edge() {
+    public Edge() { // Edge constructor - no arguments
         source = new Vertex();
         target = new Vertex();
         parent = new Vertex();
         weight = 0;
     }
 
-    public Edge(Vertex source, Vertex target, int weight) {
+    public Edge(Vertex source, Vertex target, int weight) { // constructor that initializes source, target, and weight
         this.source = source;
         this.target = target;
         this.weight = weight;
     }
 
     @Override
-    public int compareTo(Edge e) { // compare based on weights
+    public int compareTo(Edge e) { // compare based on weights and return appropriate value
         if (this.weight > e.weight) {
-            return 1;
+            return 1; 
         } else if (this.weight == e.weight) {
             return 0;
         } else {
@@ -39,11 +39,11 @@ public class Edge implements Comparable<Edge> {
         }
     }
 
-    public int parent(int pos) { // return the parent of the edge's node in the heap
+    public int parent(int pos) { // return the postion of the edge's node in the heap
         return pos / 2;
     }
 
-    public void displayInfo() {
+    public void displayInfo() { // prints information is specific format
         System.out.print("Office No. " + source.label);
         System.out.print(" - ");
         System.out.print("Office No. " + target.label);
